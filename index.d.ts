3b08690ac772ebc163d502176052d06164b1ad38
Benedek Heilig <benecene@gmail.com>
2023-02-11 00:10:20 +0100

    fix: add types for colors

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,6 @@
 export function identicon(username: string, saturation?: number|string, lightness?: number|string): string
+export function colors(username: string, saturation?: number|string, lightness?: number|string): {
+    color: string
+    lightColor: string
+    darkColor: string
+}
